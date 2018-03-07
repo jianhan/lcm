af4517a99e667504126f2fae7497b34cdbd29fa9
Jian Han <jianhan@mail.com>
2018-03-07 19:29:23 +1000

    [REFACTOR] Remove manually maessage, auto generate message is good enough

--- app/Api/Requests/UploadFile.php
+++ app/Api/Requests/UploadFile.php
@@ -36,16 +36,6 @@ class UploadFile extends FormRequest
      */
     public function messages()
     {
-        $fileName = '';
-        if ($this->request->get('file', false)) {
-            $fileName = $this->request->get('file')->getClientOriginalName();
-        }
-        return [
-            'file.required' => 'File is required',
-            'file.mimes' => 'File ' . $fileName . ' must be a image of jpg,jepg,png or gif',
-            'file.max' => 'File ' . $fileName . ' must not be over 1MB',
-            'dir.required' => 'Dir parameter is required',
-            'dir.string' => 'Dir parameter must be a string'
-        ];
+        return [];
     }
 }
